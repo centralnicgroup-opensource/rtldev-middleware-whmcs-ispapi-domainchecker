ab3433379fa68409bee06971276918127ea91591
Anthony Schneider <anthonys@hexonet.net>
2018-03-12 07:38:02 +0000

    currency was missing when customer was not logged in

--- modules/addons/ispapidomaincheck/domain.php
+++ modules/addons/ispapidomaincheck/domain.php
@@ -17,21 +17,36 @@ if(file_exists($backorder_module)){
 	require_once $backorder_module;
 }
 
+/*
+ * Returns the selected currency
+ */
+function getSelectedCurrency(){
+	//first take the currency from the URL or from the session
+	$currency = isset($_REQUEST["currency"]) ? $_REQUEST["currency"] : $_SESSION["currency"];
+
+	//if customer logged in, set the configured currency.
+	$ca = new WHMCS_ClientArea();
+	if ($ca->isLoggedIn()) {
+		$user = Helper::SQLCall("SELECT currency FROM tblclients WHERE id=?", array($ca->getUserID()));
+		$currency = $user["currency"];
+	}
+
+	//not logged in, no currency in the URL and no currency in the SESSION
+	if(empty($currency)){
+		$default = Helper::SQLCall("SELECT id FROM tblcurrencies WHERE `default`=1", array());
+		$currency = $default["id"];
+	}
+	return $currency;
+}
+
 //load all the ISPAPI registrars
 $_SESSION["ispapi_registrar"] = (new LoadRegistrars)->getLoadedRegistars();
 
 //take the action from the URL
 $action = isset($_REQUEST["action"]) ? $_REQUEST["action"] : "";
 
-//take the currency from the URL or the session
-$currency = isset($_REQUEST["currency"]) ? $_REQUEST["currency"] : $_SESSION["currency"];
-
-//if customer logged in, set the configured currency.
-$ca = new WHMCS_ClientArea();
-if ($ca->isLoggedIn()) {
-	$user = Helper::SQLCall("SELECT currency FROM tblclients WHERE id=?", array($ca->getUserID()));
-	$currency = $user["currency"];
-}
+//get the selected currency
+$currency = getSelectedCurrency();
 
 $domains = (isset($_REQUEST["domains"])) ? $_REQUEST["domains"] : "";
 
