a1716d7639b1de4b53f318f25a6276d9d2904855
Anthony Schneider <anthonys@hexonet.net>
2018-03-13 14:52:16 +0000

    some language changes

--- modules/addons/ispapidomaincheck/lang/english.php
+++ modules/addons/ispapidomaincheck/lang/english.php
@@ -1,32 +1,29 @@
 <?php
-$_LANG['backorder_info_text'] = "is registered, but you still want this domain if and when it expires. Place a free BACKORDER today and we will notify and attempt to register* when it becomes available. ";
-$_LANG['backorder_info_text_list'] = "<li><i >Registration attempt executed only for active accounts with enough positive prepaid funds at the time of expiration</i></li><li><i >Domains with multiple backorders are sent to private auction</i></li>";
-$_LANG['backorder_info_text_backorderprice'] = "Successful individual backorder registration fee is ";
-$_LANG['categories_label'] = "CATEGORIES";
-$_LANG['checkoutbutton'] = "CHECKOUT";
-$_LANG['domaincheckeravailable'] = "AVAILABLE";
-$_LANG['domaincheckertaken'] = "TAKEN";
-$_LANG['domaincheckerdomainexample'] = "Search for your domain...";
-$_LANG['whoisresults'] = "WHOIS Results for";
 $_LANG['go_search'] = "GO";
-$_LANG['price_of_domain_text'] = " upon successful registration";
-$_LANG['add_to_cart_button'] = "Add to cart";
-$_LANG['premium'] = "PREMIUM";
-$_LANG['domain_description_available'] = "This is a registry premium domain. It is classified differently than a standard domain which may affect its pricing.";
-$_LANG['domain_description_backorder'] = "using our backorder system, we will attempt to register the domain as soon as it becomes available.";
-$_LANG['domain_description_taken'] = "But not to worry, we have other domains you might be interested in. Check them out!";
-$_LANG['renewal'] = "Renewal";
-$_LANG['backorder'] = "BACKORDER";
-$_LANG['whois'] = "WHOIS";
+$_LANG['domaincheckertaken'] = "TAKEN";
+$_LANG['domaincheckeravailable'] = "AVAILABLE";
+$_LANG['categories_label'] = "CATEGORIES";
 $_LANG['more'] = "More";
+$_LANG['backorder_info_text'] = "is registered, but you still want this domain if and when it expires. Place a free BACKORDER today and we will notify and attempt to register* when it becomes available. ";
+$_LANG['backorder_info_text_list'] = "<li><i>Registration attempt executed only for active accounts with enough positive prepaid funds at the time of expiration</i></li><li><i>Domains with multiple backorders are sent to private auction</i></li>";
+$_LANG['backorder_info_text_backorderprice'] = "Successful individual backorder registration fee is ";
+$_LANG['premium'] = "PREMIUM";
+$_LANG['backorder'] = "BACKORDER";
+$_LANG['domaincheckerdomainexample'] = "Search for your domain...";
 $_LANG['domain_added_to_cart'] = "Added";
+$_LANG['renewal'] = "Renewal";
+$_LANG['whois'] = "WHOIS";
+$_LANG['domain_description_backorder'] = "using our backorder system, we will attempt to register the domain as soon as it becomes available.";
+$_LANG['price_of_domain_text'] = " upon successful registration";
+$_LANG['domain_description_taken'] = "But not to worry, we have other domains you might be interested in. Check them out!";
+$_LANG['domain_description_available'] = "This is a registry premium domain. It is classified differently than a standard domain which may affect its pricing.";
+$_LANG['add_to_cart_button'] = "Add to cart";
+$_LANG['checkoutbutton'] = "CHECKOUT";
+$_LANG['whoisresults'] = "WHOIS Results for";
 $_LANG['backorder_deleted'] = "Backorder successfully deleted.";
 $_LANG['login_required'] = "Login Required";
 $_LANG['error_occured'] = "An error occurred";
 $_LANG['backorder_created'] = "Backorder successfully created.";
-
-
-//The following are not used in tpl file
 $_LANG['add_to_cart'] = "The domain has been added to the cart.";
 $_LANG['remove_from_cart'] = "The domain has been removed from the cart.";
 $_LANG['domain_not_supported_feedback'] = "Currently we do not support this domain";
@@ -35,5 +32,4 @@ $_LANG['domain_taken_feedback'] = "This domain is not available";
 $_LANG['domain_available_feedback'] = "Your domain is available!";
 $_LANG['error_feedback'] = "An error occured, please contact the support.";
 $_LANG[''] = "";
-
 ?>
