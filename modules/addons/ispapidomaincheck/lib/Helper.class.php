70c712ffd01eab541b149f3412bd12d2e1736042
Anthony Schneider <anthonys@hexonet.net>
2018-03-13 10:58:22 +0000

    fix in the Helper class

--- modules/addons/ispapidomaincheck/lib/Helper.class.php
+++ modules/addons/ispapidomaincheck/lib/Helper.class.php
@@ -44,9 +44,9 @@ class Helper
     		}
         } catch (\Exception $e) {
             if($debug){
-                echo json_encode( array("feedback" => array( "f_type" => "sqlerror", "f_message" => $this->getTranslation("error_feedback"), "sqlmessage" => $e->getMessage(), "sqlquery" => $sql) ) );
+                echo json_encode( array("feedback" => array( "f_type" => "sqlerror", "f_message" => "An error occured, please contact the support."), "sqlmessage" => $e->getMessage(), "sqlquery" => $sql) ) );
             }else{
-                echo json_encode( array("feedback" => array( "f_type" => "sqlerror", "f_message" => $this->getTranslation("error_feedback")) ) );
+                echo json_encode( array("feedback" => array( "f_type" => "sqlerror", "f_message" => "An error occured, please contact the support.") ) );
             }
             die();
         }
