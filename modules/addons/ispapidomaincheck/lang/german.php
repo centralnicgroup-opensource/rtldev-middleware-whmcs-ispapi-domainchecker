8457cd936c14da99e5323d41018ae8fdd5e9b96f
Anthony Schneider <anthonys@hexonet.net>
2018-03-13 14:43:52 +0000

    some changes

--- modules/addons/ispapidomaincheck/lang/german.php
+++ modules/addons/ispapidomaincheck/lang/german.php
@@ -5,7 +5,7 @@ $_LANG['domaincheckeravailable'] = "VERFÜGBAR";
 $_LANG['categories_label'] = "KATEGORIEN";
 $_LANG['more'] = "Mehr";
 $_LANG['backorder_info_text'] = "ist registriert, aber Sie wollen diese Domain immer noch, wenn sie abläuft. Setzen Sie einen kostenlosen BACKORDER noch heute und wir werden Sie benachrichtigen und versuchen die Domain zu registrieren, wenn sie verfügbar ist. ";
-$_LANG['backorder_info_text_list'] = "<li><i >Registrierungsversuch wird nur für aktive Konten mit ausreichend positiven Prepaid-Fonds zum Zeitpunkt des Verfalls durchgeführt</i></li><li><i >Domains mit mehreren Backorder werden zur privaten Auktion geschickt</i></li>";
+$_LANG['backorder_info_text_list'] = "<li><i>Registrierungsversuch wird nur für aktive Konten mit ausreichend positiven Prepaid-Fonds zum Zeitpunkt des Verfalls durchgeführt</i></li><li><i >Domains mit mehreren Backorder werden zur privaten Auktion geschickt</i></li>";
 $_LANG['backorder_info_text_backorderprice'] = "Erfolgreiche individuelle Backordergebühr ist ";
 $_LANG['premium'] = "PREMIUM";
 $_LANG['backorder'] = "BACKORDER";
@@ -18,20 +18,15 @@ $_LANG['price_of_domain_text'] = " nach erfolgreicher Registrierung";
 $_LANG['domain_description_taken'] = "Aber keine Sorge, wir haben andere Domains, an denen Sie interessiert sein könnten!";
 $_LANG['domain_description_available'] = "Dies ist eine Aftermarket-Premium-Domain. Es kann einige Stunden oder bis zu einem Tag dauern, bis der Registrierungsvorgang für diese Domain abgeschlossen ist.";
 $_LANG['add_to_cart_button'] = "Zum Warenkorb hinzufügen";
-
-//The following translations need to be checked again TODO
-$_LANG['checkoutbutton'] = "AUSCHECKEN"; //not sure KASSE?
+$_LANG['checkoutbutton'] = "Zur Kasse";
 $_LANG['whoisresults'] = "WHOIS Ergebnisse für";
 $_LANG['backorder_deleted'] = "Backorder erfolgreich gelöscht.";
 $_LANG['login_required'] = "Anmeldung erforderlich";
 $_LANG['error_occured'] = "Ein Fehler ist aufgetreten";
-$_LANG['backorder_created'] = "Backorder Erfolgreich erstellt.";
-
-
-//The following are not used in tpl file
+$_LANG['backorder_created'] = "Backorder erfolgreich erstellt.";
 $_LANG['add_to_cart'] = "The domain has been added to the cart.";
 $_LANG['remove_from_cart'] = "The domain has been removed from the cart.";
-$_LANG['domain_not_supported_feedback'] = "Currently we do not support this domain";
+$_LANG['domain_not_supported_feedback'] = "Wir unterstützen diese Domain derzeit nicht";
 $_LANG['backorder_available_feedback'] = "This domain is registered but it can still be yours!";
 $_LANG['domain_taken_feedback'] = "This domain is not available";
 $_LANG['domain_available_feedback'] = "Your domain is available!";
