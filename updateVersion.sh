30f81e2949711ef0157120a212cdff183af25b49
Kai Schwarz <kschwarz@hexonet.net>
2018-10-17 09:53:10 +0200

    chore(versioning): update bash script

--- updateVersion.sh
+++ updateVersion.sh
@@ -4,15 +4,10 @@
 # IT WILL BE EXECUTED IN STEP "prepare" OF
 # semantic-release. SEE package.json
 
-# git version format: vX.Y.Z
-# in-file version format: X.Y.Z
-# command line argument version format: X.Y.Z
+# version format: X.Y.Z
 newversion="$1";
 branch="$2";
 
 if [ "$branch" = "master" ]; then
-    oldversion="$(git fetch origin --tags >/dev/null && git describe --abbrev=0 --tags)";
-    #remove leading "v" of tag version
-    oldversion="${oldversion:1}";
-    sed -i 's/'"$oldversion"'/'"$newversion"'/g' modules/addons/ispapidomaincheck/ispapidomaincheck.php
+    sed -i "s/\$module_version = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\$module_version = \"${newversion}\"/g" modules/addons/ispapidomaincheck/ispapidomaincheck.php
 fi;
