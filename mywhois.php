3a75333a91e0db9d37bed1a3ce53cc66176678b4
Kai Schwarz <kschwarz@hexonet.net>
2019-10-09 13:54:57 +0200

    fix(mywhois.php): review loading of init.php

--- mywhois.php
+++ mywhois.php
@@ -1,24 +1,10 @@
 <?php
 use ISPAPI\DCHelper;
 
-$root_path = $_SERVER["DOCUMENT_ROOT"];
-$script_path = preg_replace("/.modules.addons..+$/", "", dirname($_SERVER["SCRIPT_NAME"]));
-if (!empty($script_path)) {
-    $root_path .= $script_path;
-}
-$init_path = implode(DIRECTORY_SEPARATOR, array($root_path,"init.php"));
-if (isset($GLOBALS["customadminpath"])) {
-    $init_path = preg_replace("/(\/|\\\)" . $GLOBALS["customadminpath"] . "(\/|\\\)init.php$/", DIRECTORY_SEPARATOR . "init.php", $init_path);
-}
-if (file_exists($init_path)) {
-    require_once($init_path);
-} else {
-    exit("cannot find init.php");
-}
+require "init.php";
 
-require_once(implode(DIRECTORY_SEPARATOR, array(ROOTDIR,"includes","registrarfunctions.php")));
-require_once(implode(DIRECTORY_SEPARATOR, array(dirname(__FILE__), "modules", "addons", "ispapidomaincheck", "lib","Common","DCHelper.class.php")));
-
+require_once(implode(DIRECTORY_SEPARATOR, array(ROOTDIR, "includes", "registrarfunctions.php")));
+require_once(implode(DIRECTORY_SEPARATOR, array(ROOTDIR, "modules", "addons", "ispapidomaincheck", "lib", "Common", "DCHelper.class.php")));
 
 $idn = strtolower($_REQUEST["idn"]);
 $pc = strtolower($_REQUEST["pc"]);
