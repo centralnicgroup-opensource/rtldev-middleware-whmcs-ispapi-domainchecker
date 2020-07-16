d2f678ecd16c7a7b1013de1325491e2494e1b69f
Kai Schwarz <kschwarz@hexonet.net>
2020-07-16 14:46:17 +0200

    build(versionupdater): safe sed usage in script

--- updateVersion.sh
+++ updateVersion.sh
@@ -5,9 +5,14 @@
 # semantic-release. SEE package.json
 
 # version format: X.Y.Z
-newversion="$1";
-date="$(date +'%Y-%m-%d')";
+newversion="$1"
+date="$(date +'%Y-%m-%d')"
 
-sed -i "s/\"version\" => \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\" => \"${newversion}\"/g" modules/addons/ispapidomaincheck/ispapidomaincheck.php
-sed -i "s/\"version\": \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\": \"${newversion}\"/g" release.json
-sed -i "s/\"date\": \"[0-9]\+-[0-9]\+-[0-9]\+\"/\"date\": \"${date}\"/g" release.json
+printf -v sed_script 's/"version" => "[0-9]\+\.[0-9]\+\.[0-9]\+"/"version" => "%s"/g' "${newversion}"
+sed -i -e "${sed_script}" modules/addons/ispapidomaincheck/ispapidomaincheck.php
+
+printf -v sed_script 's/"version": "[0-9]\+\.[0-9]\+\.[0-9]\+"/"version": "%s"/g' "${newversion}"
+sed -i -e "${sed_script}" release.json
+
+printf -v sed_script 's/"date": "[0-9]\+-[0-9]\+-[0-9]\+"/"date": "%s"/g' "${date}"
+sed -i -e "${sed_script}" release.json
