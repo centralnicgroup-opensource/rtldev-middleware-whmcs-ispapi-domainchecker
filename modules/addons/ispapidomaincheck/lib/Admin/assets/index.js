36971eecfe2862f506b82c6627520f7f5df8b485
Kai Schwarz <kschwarz@hexonet.net>
2021-10-06 19:50:55 +0200

    fix(aa settings): fixed pre-configured search url

--- modules/addons/ispapidomaincheck/lib/Admin/assets/index.js
+++ modules/addons/ispapidomaincheck/lib/Admin/assets/index.js
@@ -883,7 +883,7 @@ function generateTab1Block1() {
 	const baseurl = `${url.origin}${url.pathname.replace(
 		/[^/]+\/[^/]+$/,
 		'',
-	)}mydomainchecker.php?search=mydomain.com&`;
+	)}domainchecker.php?search=mydomain.com&`;
 	url = `${baseurl}cat=${data.defaultActiveCategories}`;
 	$uri.text(url);
 	$uri.prop('href', url);
