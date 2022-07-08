b100b04c2715bbd3dd85d3e6ef4c9de36e38c089
Kai Schwarz <kschwarz@hexonet.net>
2022-07-08 10:07:49 +0200

    build(npm): remove phpcompatibility script

--- scripts/phpcompatibility.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-PHP_VERSION="$(php -r 'echo PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;')"
-phpcs --standard=PHPCompatibility -q -n --colors --runtime-set testVersion "$PHP_VERSION" modules mydomainchecker.php mywhois.php || exit 1;
